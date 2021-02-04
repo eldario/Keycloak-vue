dbd91ef590d0bc877cfea60a51f43158706a9bf2
Eldario <eldario666@gmail.com>
2021-02-05 01:49:25 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# hello-keycloak
+
+## Project setup
+```
+$ docker-composer up
+```
+
+## Links
+
+`http://127.0.0.1:9901` - Web UI
+
+`http://127.0.0.1:9902` - KeyCloak Web UI
---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import babel from '@rollup/plugin-babel'
+import pkg from './package.json'
+
+const version = process.env.VERSION || require('./package.json').version
+const banner =
+`/*!
+  * vue-keycloak-js v${version}
+  * @license ISC
+  */`
+const name = 'dsb-vue-keycloak'
+
+// CommonJS (for Node), ES module (for bundlers) and browser-friendly UMD build.
+export default {
+  input: 'src/index.js',
+  output: [
+    { file: pkg.main, format: 'cjs', banner, name },
+    { file: pkg.module, format: 'es', banner, name },
+    { file: pkg.browser, format: 'umd', banner, name }
+  ],
+  plugins: [
+    resolve(), // so Rollup can find `keycloak-js`
+    commonjs(), // so Rollup can convert `keycloak-js` to an ES module
+    babel({
+      exclude: ['node_modules/**'],
+      babelHelpers: 'bundled'
+    })
+  ]
+}
+
